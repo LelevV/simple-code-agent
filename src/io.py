e8e7d119319099ebe07da83d897281976536f693
Leon <leon.vanveldhuijzen@gmail.com>
2026-05-24 19:57:17 +0200

    add io tools

---
+++ src/io.py
@@ -0,0 +1,14 @@
+import os
+
+
+def read_file(path):
+    with open(path, 'r') as f:
+        return f.read()
+    
+
+def list_files(project_dir):
+    file_list = []
+    for root, dirs, files in os.walk(project_dir):
+        for file in files:
+            file_list.append(os.path.relpath(os.path.join(root, file), project_dir))
+    return file_list
